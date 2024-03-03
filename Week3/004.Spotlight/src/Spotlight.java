4f7224b7d0782f2dd73e12ded7ae476c21473e05
timsparrie <timsparreboom@hotmail.com>
2024-03-03 09:07:59 +0100

    spotlighht gefixt

--- Week3/004.Spotlight/src/Spotlight.java
+++ Week3/004.Spotlight/src/Spotlight.java
@@ -1,25 +1,24 @@
-import java.awt.*;
-import java.awt.geom.*;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-
-import static javafx.application.Application.launch;
-
-import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-
-import javax.imageio.ImageIO;
-
 import org.jfree.fx.FXGraphics2D;
 import org.jfree.fx.ResizableCanvas;
 
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Ellipse2D;
+import java.util.Random;
+
 public class Spotlight extends Application {
     private ResizableCanvas canvas;
+    private int mouseX;
+    private int mouseY;
+    private Random r = new Random();
+    private Shape shape;
+
 
     @Override
     public void start(Stage stage) throws Exception
@@ -29,6 +28,7 @@ public class Spotlight extends Application {
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
         mainPane.setCenter(canvas);
         FXGraphics2D g2d = new FXGraphics2D(canvas.getGraphicsContext2D());
+
         new AnimationTimer() {
             long last = -1;
 
@@ -45,8 +45,10 @@ public class Spotlight extends Application {
 
         stage.setScene(new Scene(mainPane));
         stage.setTitle("Spotlight");
+
         stage.show();
         draw(g2d);
+
     }
 
 
@@ -55,20 +57,35 @@ public class Spotlight extends Application {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+        graphics.setClip(0,0, (int) canvas.getWidth(), (int) canvas.getHeight());
+
+        shape = new Ellipse2D.Double(mouseX -100, mouseY-100, 200,200);
+        graphics.draw(shape);
+        graphics.clip(shape);
+        for(int i = 0; i < 1000; i++) {
+            graphics.setPaint(Color.getHSBColor(r.nextFloat(), 1, 1));
+            graphics.drawLine((int) (r.nextInt() % canvas.getWidth()), (int) (r.nextInt() % canvas.getHeight()),
+                    (int) (r.nextInt() % canvas.getWidth()), (int) (r.nextInt() % canvas.getHeight()));
+        }
+
+
+        graphics.setClip(0,0, (int) canvas.getWidth(), (int) canvas.getHeight());
+
+
+
+
     }
 
-    public void init()
-    {
-
+    public void update(double deltaTime) {
+        canvas.setOnMouseMoved(e -> mouseLocation(e));
     }
 
-    public void update(double deltaTime)
-    {
-
+    private void mouseLocation(MouseEvent e) {
+        mouseX = (int) e.getX();
+        mouseY = (int) e.getY();
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         launch(Spotlight.class);
     }
 
