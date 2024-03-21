e2cbf19d75c3bcc0e4f138548feb66ee1d3bab9b
timsparrie <timsparreboom@hotmail.com>
2024-03-21 14:52:34 +0100

    Completed Mirror

--- Week2/005.Mirror/src/Mirror.java
+++ Week2/005.Mirror/src/Mirror.java
@@ -1,16 +1,14 @@
-import java.awt.*;
-import java.awt.geom.*;
-
 import javafx.application.Application;
-
-import static javafx.application.Application.launch;
-
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 import org.jfree.fx.ResizableCanvas;
+
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Area;
+import java.awt.geom.Rectangle2D;
 
 public class Mirror extends Application {
     ResizableCanvas canvas;
@@ -30,9 +28,28 @@ public class Mirror extends Application {
 
     public void draw(FXGraphics2D graphics)
     {
-        graphics.setTransform(new AffineTransform());
+        AffineTransform affineTransform = new AffineTransform();
+        graphics.setTransform(affineTransform);
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+        graphics.translate(canvas.getWidth()/2, canvas.getHeight()/2);
+        graphics.scale(1,-1);
+        graphics.drawLine((int) (-canvas.getWidth()/2), 0, (int) (canvas.getWidth()/2), 0);
+        graphics.drawLine(0, (int) (-canvas.getHeight()/2), 0, (int) (canvas.getHeight()/2));
+        int lastY = (int) (-canvas.getWidth()/2);
+
+        for (int x = (int) (-canvas.getWidth()/2); x < canvas.getHeight()/2; x++) {
+            int y = (int) (2.5*x);
+            graphics.drawLine(x,y,x-1,lastY);
+            lastY = y;
+        }
+        Area block = new Area(new Rectangle2D.Double(-50,100, 100,100));
+        graphics.draw(block);
+        AffineTransform tx = new AffineTransform(2/7.25-1,5/7.25,5/7.25,12.5/7.25-1 ,0,0 );
+        block.transform(tx);
+        graphics.draw(block);
+
+
     }
 
 
