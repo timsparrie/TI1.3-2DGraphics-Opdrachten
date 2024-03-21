135a11129d9cfdfb1f1001b56079570a64b15708
timsparrie <timsparreboom@hotmail.com>
2024-03-21 15:46:32 +0100

    Completed Blockdrag

--- Week2/006.BlockDrag/src/BlockDrag.java
+++ Week2/006.BlockDrag/src/BlockDrag.java
@@ -1,3 +1,5 @@
+import com.sun.javafx.geom.Point2D;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
@@ -8,25 +10,44 @@ import org.jfree.fx.ResizableCanvas;
 
 import java.awt.*;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
 
 public class BlockDrag extends Application {
     ResizableCanvas canvas;
+    private ArrayList<Renderable> renderables = new ArrayList<>();
 
     @Override
-    public void start(Stage primaryStage) throws Exception
-    {
+    public void start(Stage primaryStage) throws Exception {
+//        init();
         BorderPane mainPane = new BorderPane();
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
         mainPane.setCenter(canvas);
         primaryStage.setScene(new Scene(mainPane));
         primaryStage.setTitle("Block Dragging");
         primaryStage.show();
+        FXGraphics2D g2d = new FXGraphics2D(canvas.getGraphicsContext2D());
+        new AnimationTimer() {
+            long last = -1;
+
+            @Override
+            public void handle(long now)
+            {
+                if (last == -1)
+                    last = now;
+                update((now - last) / 1000000000.0);
+                last = now;
+                draw(g2d);
+            }
+        }.start();
+
 
         canvas.setOnMousePressed(e -> mousePressed(e));
         canvas.setOnMouseReleased(e -> mouseReleased(e));
         canvas.setOnMouseDragged(e -> mouseDragged(e));
 
-        draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
+        init();
+        draw(g2d);
     }
 
 
@@ -35,6 +56,17 @@ public class BlockDrag extends Application {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+        for (Renderable rendereble: renderables) {
+            rendereble.draw(graphics);
+        }
+
+    }
+    public void init() {
+        renderables.add(new Renderable(new Rectangle2D.Double(-50,-50,100,100), new Point2D(134,343),0, 1));
+        renderables.add(new Renderable(new Rectangle2D.Double(-50,-50,100,100), new Point2D(188,122),0, 1));
+    }
+    public void update (double deltatime) {
+
     }
 
 
@@ -43,17 +75,26 @@ public class BlockDrag extends Application {
         launch(BlockDrag.class);
     }
 
-    private void mousePressed(MouseEvent e)
-    {
-    }
+    private void mousePressed(MouseEvent e) {
 
-    private void mouseReleased(MouseEvent e)
-    {
 
     }
 
-    private void mouseDragged(MouseEvent e)
-    {
+    private void mouseReleased(MouseEvent e) {
+
+    }
+
+    private void mouseDragged(MouseEvent e) {
+        boolean dragged = false;
+        for (Renderable renderable: renderables) {
+            if (renderable.inBlock(new Point2D((float) e.getX(), (float) e.getY())) && !dragged) {
+                renderable.setPosition(new Point2D((float) e.getX(), (float) e.getY()));
+                dragged = true;
+            }
+
+        }
+
+
     }
 
 }
---
+++ Week2/006.BlockDrag/src/Renderable.java
@@ -0,0 +1,50 @@
+import com.sun.javafx.geom.Point2D;
+import org.jfree.fx.FXGraphics2D;
+
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+
+public class Renderable {
+
+        private Shape shape;
+        private Point2D position;
+        private float rotation;
+        private float scale;
+
+        public Renderable(Shape shape, Point2D position, float rotation, float scale) {
+            this.shape = shape;
+            this.position = position;
+            this.rotation = rotation;
+            this.scale = scale;
+        }
+
+        public void draw(FXGraphics2D g2d) {
+            g2d.draw(getTransformedShape());
+        }
+
+        public Shape getTransformedShape() {
+            return getTransform().createTransformedShape(shape);
+        }
+
+        public void setPosition(Point2D position) {
+            this.position = position;
+        }
+        public boolean inBlock(Point2D position) {
+            boolean state = false;
+            if ((position.x > this.position.x-50 && position.x < this.position.x+50)
+                    && position.y > this.position.y-50 && position.y < this.position.y+50) {
+                state = true;
+            }
+            return state;
+        }
+
+        public AffineTransform getTransform() {
+            AffineTransform tx = new AffineTransform();
+            tx.translate(position.x, position.y);
+            tx.rotate(rotation);
+            tx.scale(scale,scale);
+            return tx;
+        }
+
+
+}
